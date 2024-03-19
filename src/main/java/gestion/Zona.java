79f729e473aae9cb9170ef075ba6998d80e478c6
Uribej2711 <juuribelo@unal.edu.co>
2024-03-19 11:31:24 -0500

    a
    a

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,55 +1,54 @@
 package gestion;
 
 import java.util.ArrayList;
-
 import zooAnimales.Animal;
 
 public class Zona {
 	private String nombre;
 	private Zoologico zoo;
-	private ArrayList<Animal>animales= new ArrayList<>();
+	private ArrayList<Animal> animales;
+	
+	public Zona(String nombre, Zoologico zoo) {
+		this.nombre = nombre;
+		this.zoo = zoo;
+		this.animales = new ArrayList<Animal>();
+	}
 	
 	public Zona() {
-		this(null,null);
+		this.animales = new ArrayList<Animal>();
 	}
 	
-	public Zona(String nombre,Zoologico zoo) {
-		this.nombre=nombre;
-		this.zoo=zoo;
-		
+	public void agregarAnimales(Animal animal) {
+		this.animales.add(animal);
 	}
 	
+	public int cantidadAnimales() {
+		int cantAnim = 0;
+		for(Animal animal : animales) {
+			if (animal != null) {
+			cantAnim += 1;
+			}
+		}
+		return cantAnim;
+	}
 	
 	public void setNombre(String nombre) {
-		this.nombre=nombre;
+		this.nombre = nombre;
 	}
 	public String getNombre() {
-		return nombre;
+		return this.nombre;
 	}
-	
 	public void setZoo(Zoologico zoo) {
-		this.zoo=zoo;
+		this.zoo = zoo;
 	}
 	public Zoologico getZoo() {
-		return zoo;
+		return this.zoo;
 	}
-	
-	public void setAnimales(ArrayList<Animal> animales) {
-		this.animales=animales;
+	public void setAnimales(ArrayList<Animal>animales) {
+		this.animales = animales;
 	}
-	public  ArrayList<Animal> getAnimales() {
-		return animales;
+	public ArrayList<Animal> getAnimales() {
+		return this.animales;
 	}
-	
-	
-    public void agregarAnimales(Animal animal) {
-    	animales.add(animal);
-    	
-    }
-    public int cantidadAnimales() {
-    	int cantAnimales=animales.size();
-    	return cantAnimales;
-    	
-    }
 
 }
